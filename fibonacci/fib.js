d85a9f8f402cda00abf7a192459e7f9a0915991f
guudewie <philippthiessen12@gmail.com>
2023-09-17 18:13:00 +0200

    add fibonacci function with recursion

--- fibonacci/fib.js
+++ fibonacci/fib.js
@@ -14,3 +14,17 @@ function fibs (n) {
     }
     return a
 }
+
+// FIBONACCI SEQUENCE RECURSION
+
+function fibsRecOneLine (n) {
+    return n === 1 ? [0] : n === 2 ? [0, 1] : [...fibsRec(n-1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]]
+}
+
+function fibsRec (n) {
+    
+    if ( n == 1 ) return [ 0 ]
+    if ( n == 2 ) return [ 0, 1 ]
+    
+    return [...fibsRec(n-1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]]
+}
