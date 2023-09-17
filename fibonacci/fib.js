89a078bf5c7956c5e1e15696778a18b7743babc9
guudewie <philippthiessen12@gmail.com>
2023-09-17 12:18:49 +0200

    finish fib sequence function

--- fibonacci/fib.js
+++ fibonacci/fib.js
@@ -1,3 +1,16 @@
-#!/usr/bin/node
+// install node and run following command in terminal >>> $node fibonacci/fib.js 
 
-console.log("Hello")
+// FIBONACCI SEQUENCE
+
+function fibs (n) {
+
+    if (n === 0) return []
+    if (n === 1) return [0]
+    
+    let a = [0, 1];
+
+    for (let i = 0; i <= n - 3; i++) {
+        a.push( a[a.length - 1] + a[a.length - 2])
+    }
+    return a
+}
